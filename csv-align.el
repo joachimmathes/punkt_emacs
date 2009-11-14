bc619a9658a2d7dcbf33cc7ee3279db9031b4f8a
Joachim Mathes <joachim_mathes@web.de>
2009-11-14 15:49:23 +0100

    Add comment header

--- csv-align.el
+++ csv-align.el
@@ -1,7 +1,42 @@
+;;; csv-align.el --- A collection of usefull functions
+
+;; Copyright (C) 2009  Joachim Mathes
+;;
+;; Author: Joachim Mathes <joachim <underscore> mathes <at> web <dot> de>
+;; Created: November 2009
+;; Version: $Id$
+;; Keywords: files
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary
+
+;; This is a collection of useful functions.
+;; Every function is prefixed with `joma' which refers to my name and is used
+;; as a namespace.
+
+;; This file is *NOT* part of GNU Emacs.
+
+;;; Code:
 (defun joma-csv-align (separator)
-  "Aligns columns of a buffer visting a csv file. The SEPARATOR parameter
-specifies the column separator. The default value is a comma `,'"
+  "Align columns of a buffer visting a csv file.
+The SEPARATOR parameter specifies the column separator.  The default value is a
+comma `,'"
   (interactive "cCSV Separator:")
+  (princ "Operating...")
   (let ((formatter (list))
         (line-elements-lengths (list)))
     (save-excursion
@@ -48,4 +83,9 @@ specifies the column separator. The default value is a comma `,'"
 	  (if (< element-position (1- (length line-elements)))
 	      (insert (char-to-string separator)))
     	  (setq element-position (1+ element-position)))
-        (forward-line))))))
+        (forward-line)))))
+  (princ "Operating...done"))
+
+(provide 'csv-align)
+
+;;; csv-align.el ends here
